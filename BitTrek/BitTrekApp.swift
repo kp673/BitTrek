9b440193d46a380f2c95568e16612f3a8763f206
Kush Patel <kush161100@gmail.com>
2025-03-21 18:54:44 -0400

    Initial Commit

---
+++ BitTrek/BitTrekApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitTrekApp.swift
+//  BitTrek
+//
+//  Created by Kush Patel on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BitTrekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitTrek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BitTrek
+//
+//  Created by Kush Patel on 3/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
